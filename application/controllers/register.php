1492e3cbed2f42b9bd552e6077b9b4d001bfffcf
Garrick Plaisted <gp@garrickplasited.com>
2013-11-06 10:56:29 -0700

    Allows emails to send to 1and1.com emails and cha

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -7,34 +7,42 @@ class Register extends CI_Controller {
     }
 
     public function index(){
-
         $this->register();
+
     }
 
-    public function register(){
-
+    public function register()
+    {
         $this->load->view('register');
     }
 
     public function submit(){
 
+
+
 		// Email Settings
-		$config = Array(
-			'protocol' => 'smtp',
-            'smtp_host' => 'smtp.1and1.com',
+        $config = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp.perfora.net',
+            'smtp_user' => 'gp@garrickplaisted.com',
+            'smtp_pass' => 'nedyac0920',
             'smtp_port' => 587,
-			'smtp_user' => 'gp@garrickplaisted.com',
-			'smtp_pass'   => 'nedyac0920'
-		   );
-			   
+            'charset' => 'utf-8',
+            'mailtype' => 'html',
+            'validate' => 'FALSE',
+            'useragent' => 'none',
+            'crlf' => "\r\n",
+            'newline' => "\r\n"
+        );
+
+        $this->load->library('email', $config);
         $this->load->library('form_validation');
-		$this->load->library('email', $config);
 		$this->load->model('mdl_users');
 		
 		
         $this->form_validation->set_rules('firstname','Firstname','required|min_length[3]|xss_clean|trim');
         $this->form_validation->set_rules('lastname','Lastname','required|min_length[3]|xss_clean|trim');
-        $this->form_validation->set_rules('email','Email','required|xss_clean|valid_email|trim|is_unique[users.email]');
+        $this->form_validation->set_rules('email','Email','required|xss_clean}valid_email|trim|is_unique[users.email]');
         $this->form_validation->set_rules('username','Username','required|min_length[3]|xss_clean|trim|is_unique[users.username]');
         $this->form_validation->set_rules('password','Password','required|min_length[3]|xss_clean|trim');
         $this->form_validation->set_rules('confirm_password','Confirm Password','required|min_length[3]|xss_clean|matches[password]');
@@ -73,14 +81,15 @@ class Register extends CI_Controller {
     }
 	
 	public function confirm($key){
-
 		$this->load->model('mdl_users');
 		
 		if($this->mdl_users->is_key_valid($key)){
 			if($this->mdl_users->confirm_update($key)){
+				
 				$data['message'] = 'Awesome, you confirmed your registration! Now you can login.';
 				$this->load->view('login', $data);
 			}
+			
 		}else{
 			echo "Not valid key";
 		}
